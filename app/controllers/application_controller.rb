6636d41871856a45bd28c8c96148f5f0e8df821d
Igor Ranieri Elland <igor.ranieri@gmail.com>
2011-01-04 15:23:58 -0200

    created filter for logged in actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery 
-  helper_method :current_user  
+  helper_method :all
     
   private
   def current_user  
     @current_user ||= User.find(session[:user_id]) if session[:user_id]  
-  end  
+  end
+
+	def login_required
+		if session[:user_id].nil?
+			redirect_to :back rescue redirect_to root_path
+		end
+	end
 end
