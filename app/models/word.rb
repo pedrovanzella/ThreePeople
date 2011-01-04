28acca9e7a87aa6a73f58802bf1657785d8641ca
Igor Ranieri Elland <igor.ranieri@gmail.com>
2011-01-04 17:13:48 -0200

    sanitizing @s

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,3 +1,15 @@
 class Word < ActiveRecord::Base
   attr_accessible :name, :personone, :persontwo, :personthree
+
+	def sanitize!
+		if !personone.scan('@').empty?
+			personone = personone.sub("@", "")
+		end
+		if !persontwo.scan('@').empty?
+			persontwo = persontwo.sub("@", "")
+		end
+		if !personthree.scan('@').empty?
+			personthree = personthree.sub("@", "")
+		end
+	end
 end
