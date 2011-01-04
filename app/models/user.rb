925a0d9c961a0feabd5bbf891f66123e5ee35b85
Pedro Vanzella <pedro@pedrovanzella.com>
2011-01-04 14:42:43 -0200

    less blind

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
       user.provider = auth["provider"]  
       user.uid = auth["uid"]  
       user.name = auth["user_info"]["name"] 
-      user.token = auth['credentials']['token'],
-      user.secret = auth['credentials']['secret'] 
+      user.token = auth["credentials"]["token"],
+      user.secret = auth["credentials"]["secret"] 
     end  
   end  
 end
