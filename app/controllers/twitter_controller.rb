a4370f2d46a9be9567a95884a64a1d83d06cdd69
Pedro Vanzella <pedro@pedrovanzella.com>
2011-01-04 13:15:58 -0200

    try again

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,25 +1,27 @@
 class TwitterController < ApplicationController
 
+	def prepare_access_token(oauth_token, oauth_token_secret)
+  	consumer = OAuth::Consumer.new("laHbZLaY5KmkiEPTuP1JWw", "XVcRARDRBSeVQX2hF0zkXmKzYBVhWOJyZ2qHXlkQU",
+    {
+    	:site => "http://api.twitter.com"
+    })
+    # now create the access token object from passed values
+    token_hash =
+    {
+      :oauth_token => oauth_token,
+    	:oauth_token_secret => oauth_token_secret
+    }
+    access_token = OAuth::AccessToken.from_hash(consumer, token_hash )
+    return access_token
+	end
+
 	def tweet
-			def prepare_access_token(oauth_token, oauth_token_secret)
-            consumer = OAuth::Consumer.new("laHbZLaY5KmkiEPTuP1JWw", "XVcRARDRBSeVQX2hF0zkXmKzYBVhWOJyZ2qHXlkQU",
-                {
-                 :site => "http://api.twitter.com"
-                })
-            # now create the access token object from passed values
-            token_hash = { :oauth_token => oauth_token,
-                           :oauth_token_secret => oauth_token_secret
-                         }
-            access_token = OAuth::AccessToken.from_hash(consumer, token_hash )
-            return access_token
-        end
+		# Exchange our oauth_token and oauth_token secret for the AccessToken instance.
+    @access_token = prepare_access_token(current_user.token, current_user.secret)
 
-        # Exchange our oauth_token and oauth_token secret for the AccessToken instance.
-        @access_token = prepare_access_token(current_user.token, current_user.secret)
-
-				@response = @access_token.request(:post, "http://api.twitter.com/1/statuses/update.json", :status => "Tweet pela API")
+		@response = @access_token.request(:post, "http://api.twitter.com/1/statuses/update.json", :status => "Tweet pela API")
 				
-				render :html => @response.body
+		render :html => @response.body
 	end
 
 end
