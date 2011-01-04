665e161aa1b8cc17ac4eefe9f37888af4cea68f7
Igor Ranieri Elland <igor.ranieri@gmail.com>
2011-01-04 17:26:53 -0200

    sanitizing @s

--- app/models/word.rb
+++ app/models/word.rb
@@ -4,6 +4,7 @@ class Word < ActiveRecord::Base
 	validates_length_of :personone, :in => 1..42, :allow_nil => false
 	validates_length_of :persontwo, :in => 1..42, :allow_nil => false
 	validates_length_of :personthree, :in => 1..42, :allow_nil => false
+	before_save :sanitize!
 
 	def sanitize!
 		if !personone.scan('@').empty?
