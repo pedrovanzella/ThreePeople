aa8536b82026e866af4e0ea67d7348b48fbd8e20
Igor Ranieri Elland <igor.ranieri@gmail.com>
2011-01-05 09:14:14 -0200

    preventing too long entries

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,9 +1,9 @@
 class Word < ActiveRecord::Base
 	attr_accessible :name, :personone, :persontwo, :personthree
-	validates_length_of :name, :minimum => 3, :allow_nil => false
-	validates_length_of :personone, :minimum => 1, :allow_nil => false
-	validates_length_of :persontwo, :minimum => 1, :allow_nil => false
-	validates_length_of :personthree, :minimum => 1, :allow_nil => false
+	validates_length_of :name, :within => 3..35, :allow_nil => false
+	validates_length_of :personone, :within => 1..15, :allow_nil => false
+	validates_length_of :persontwo, :within => 1..15, :allow_nil => false
+	validates_length_of :personthree, :within => 1..15, :allow_nil => false
 	before_save :sanitize!
 
 	def sanitize!
