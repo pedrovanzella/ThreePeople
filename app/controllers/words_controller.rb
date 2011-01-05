87f476d4ae8bef274dbb6f6c9840f0228051600d
Pedro Vanzella <pedro@pedrovanzella.com>
2011-01-05 16:11:47 -0200

    fixing words controller

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,31 +1,32 @@
 class WordsController < ApplicationController
-	before_filter :login_required
-	
+  before_filter :login_required
+
   def index
-		@title = "Latest"
+    @title = "Latest"
     @words = Word.all
   end
 
   def new
-  	@title = "New Definition"
+    @title = "New Definition"
     @word = Word.new
   end
 
   def create
     @word = Word.new(params[:word])
-		
-		@word.personone = params['word']['personone'].split(' ').join
-		@word.persontwo = params['word']['persontwo'].split(' ').join
-		@word.personthree = params['word']['personthree'].split(' ').join
-		
+
+    @word.personone = params['word']['personone'].split(' ').join
+    @word.persontwo = params['word']['persontwo'].split(' ').join
+    @word.personthree = params['word']['personthree'].split(' ').join
+    @word.name = params['word']['name']
+
     if @word.save
       flash[:notice] = "Successfully created word."
-			if params['follow']
-				follow
-			end
-			if params['tweet']
-				tweet(@word)
-			end
+      if params['follow']
+        follow
+      end
+      if params['tweet']
+        tweet(@word)
+      end
       redirect_to @word
     else
       render :action => 'new'
@@ -36,35 +37,35 @@ class WordsController < ApplicationController
     @word = Word.find(params[:id])
   end
 
-private
-	def follow
-		Twitter.configure do |config|
-		  config.consumer_key = "laHbZLaY5KmkiEPTuP1JWw"
-		  config.consumer_secret = "XVcRARDRBSeVQX2hF0zkXmKzYBVhWOJyZ2qHXlkQU"
-		  config.oauth_token = current_user.token
-		  config.oauth_token_secret = current_user.secret
-		end
-		
-		client = Twitter::Client.new
-		begin
-			client.follow('elland')
-			client.follow('pedrovanzella')
-		rescue
-			
-		end
-	end
+  private
+  def follow
+    Twitter.configure do |config|
+      config.consumer_key = "laHbZLaY5KmkiEPTuP1JWw"
+      config.consumer_secret = "XVcRARDRBSeVQX2hF0zkXmKzYBVhWOJyZ2qHXlkQU"
+      config.oauth_token = current_user.token
+      config.oauth_token_secret = current_user.secret
+    end
+
+    client = Twitter::Client.new
+    begin
+      client.follow('elland')
+      client.follow('pedrovanzella')
+    rescue
+
+    end
+  end
 
 
-	def tweet(w)
-		Twitter.configure do |config|
-		  config.consumer_key = "laHbZLaY5KmkiEPTuP1JWw"
-		  config.consumer_secret = "XVcRARDRBSeVQX2hF0zkXmKzYBVhWOJyZ2qHXlkQU"
-		  config.oauth_token = current_user.token
-		  config.oauth_token_secret = current_user.secret
-		end
-		
-		client = Twitter::Client.new
-		client.update("I just said @#{params['word']['personone']}, @#{params['word']['persontwo']} and @#{params['word']['personthree']} are all #{params['word']['name']}, at http://threepeople.me")
-		
-	end
+  def tweet(w)
+    Twitter.configure do |config|
+      config.consumer_key = "laHbZLaY5KmkiEPTuP1JWw"
+      config.consumer_secret = "XVcRARDRBSeVQX2hF0zkXmKzYBVhWOJyZ2qHXlkQU"
+      config.oauth_token = current_user.token
+      config.oauth_token_secret = current_user.secret
+    end
+
+    client = Twitter::Client.new
+    client.update("I just said @#{params['word']['personone']}, @#{params['word']['persontwo']} and @#{params['word']['personthree']} are all #{params['word']['name']}, at http://threepeople.me")
+
+  end
 end
